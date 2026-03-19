1217d6ff2793ae299bbc2481d4bfb7a6fc07549f
David Poblador i Garcia <davidpoblador@users.noreply.github.com>
2026-03-19 09:18:37 +0000

    chore: update factoryfloor to v0.1.30

--- Casks/factoryfloor.rb
+++ Casks/factoryfloor.rb
@@ -1,8 +1,8 @@
 cask "factoryfloor" do
-  version "0.1.29"
-  sha256 "e374bfeecf17cdfb43bfa0a97a19a2ac4d857d504585dcc2e9e42b8e2a4176ab"
+  version "0.1.30"
+  sha256 "bae3bed64c830943a7f59e1e427b18371b99a1c51dd73629c4df91ace0a7945c"
 
-  url "https://github.com/alltuner/factoryfloor/releases/download/v0.1.29/FactoryFloor-0.1.29.dmg"
+  url "https://github.com/alltuner/factoryfloor/releases/download/v0.1.30/FactoryFloor-0.1.30.dmg"
   name "Factory Floor"
   desc "AI-powered development workspace for macOS"
   homepage "https://factory-floor.com"

02295fba3b2b05f3c87c342a94110ccca0e6d4ae
David Poblador i Garcia <davidpoblador@users.noreply.github.com>
2026-05-03 22:53:07 +0000

    vacant: update to v0.3.4

--- Formula/vacant.rb
+++ Formula/vacant.rb
@@ -1,26 +1,26 @@
 class Vacant < Formula
   desc "Fast domain availability checker via authoritative DNS"
   homepage "https://github.com/alltuner/vacant"
-  version "0.3.3"
+  version "0.3.4"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/alltuner/vacant/releases/download/v0.3.3/vacant-aarch64-apple-darwin.tar.gz"
-      sha256 "2a56f42433e2bf4c0e51e74151102dd37c3e1ba7038db18d2e3174ee29b33020"
+      url "https://github.com/alltuner/vacant/releases/download/v0.3.4/vacant-aarch64-apple-darwin.tar.gz"
+      sha256 "25216e98826b66d1e6510a503c6c55046293756ccef185c3954d734e8191c7ae"
     else
-      url "https://github.com/alltuner/vacant/releases/download/v0.3.3/vacant-x86_64-apple-darwin.tar.gz"
-      sha256 "3ef503c549537c6112ecaa76ab9def2f9e281297874d19b20ff237bf7cdfd4f9"
+      url "https://github.com/alltuner/vacant/releases/download/v0.3.4/vacant-x86_64-apple-darwin.tar.gz"
+      sha256 "be19c3abd15a2b94610b7226af8b73c000c3c9b37df9c65d73f7d20ba0191d17"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/alltuner/vacant/releases/download/v0.3.3/vacant-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "d4050eed5394b7526509ce51d19be9acbcc9b38748bffdccb40307e96851c914"
+      url "https://github.com/alltuner/vacant/releases/download/v0.3.4/vacant-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "d8e5e0f9b108de79d503672ddc0594f7b191a22ff10638f8593c233f5037e73d"
     else
-      url "https://github.com/alltuner/vacant/releases/download/v0.3.3/vacant-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "fffbb92682fea1c3c7aee7aee0434fd07eeb5692befa4644f8b4c98675865466"
+      url "https://github.com/alltuner/vacant/releases/download/v0.3.4/vacant-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "0d0959a6120ff0d05d7209f9929f035ddaf7d06c11c68ffe7529932b6c8b1dbe"
     end
   end
 

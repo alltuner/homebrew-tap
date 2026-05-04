3ed8ab1cd38951e2129b45d7986255a7123bd99e
David Poblador i Garcia <davidpoblador@users.noreply.github.com>
2026-05-04 10:47:22 +0000

    vacant: update to v0.4.2

--- Formula/vacant.rb
+++ Formula/vacant.rb
@@ -1,26 +1,26 @@
 class Vacant < Formula
   desc "Fast domain availability checker via authoritative DNS"
   homepage "https://github.com/alltuner/vacant"
-  version "0.4.1"
+  version "0.4.2"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/alltuner/vacant/releases/download/v0.4.1/vacant-aarch64-apple-darwin.tar.gz"
-      sha256 "ffe114bf3f924b6406267db0ad3e356d44e2022281ca3313303ecf681e585875"
+      url "https://github.com/alltuner/vacant/releases/download/v0.4.2/vacant-aarch64-apple-darwin.tar.gz"
+      sha256 "2dc5a0badf90cd7e2e6f36187224c105d820676e946efa35d1168d90f91c9dc2"
     else
-      url "https://github.com/alltuner/vacant/releases/download/v0.4.1/vacant-x86_64-apple-darwin.tar.gz"
-      sha256 "6e7114d480a450169a63a6bffda50746fea324cb879eea772487eaeb1d52badb"
+      url "https://github.com/alltuner/vacant/releases/download/v0.4.2/vacant-x86_64-apple-darwin.tar.gz"
+      sha256 "6400966803166318d79db9cfa108ffadc5ae564a8925ec5cae0657bca0960de3"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/alltuner/vacant/releases/download/v0.4.1/vacant-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "994d5fdd01282e03bd798e45c2c8b966fba9bdeafdc402d7e70b6ede03a7af9f"
+      url "https://github.com/alltuner/vacant/releases/download/v0.4.2/vacant-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "a11630104b87fed68827acbb045bb2e9e1674eeb4eeecfe36787ae90777a4b6e"
     else
-      url "https://github.com/alltuner/vacant/releases/download/v0.4.1/vacant-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "840dabc003a67532a11b9e56eaf518354914e194e362d1fb0966e2c29f5dbf5e"
+      url "https://github.com/alltuner/vacant/releases/download/v0.4.2/vacant-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "a1dccbba670ba0093399161daf8eefca0ee5aa00698c17c3579e5b4cd689e976"
     end
   end
 

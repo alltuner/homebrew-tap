0e098c4af5baa050fc51bdfbc3693f617e449765
David Poblador i Garcia <davidpoblador@users.noreply.github.com>
2026-05-04 13:38:59 +0000

    vacant: update to v0.4.0

--- Formula/vacant.rb
+++ Formula/vacant.rb
@@ -1,26 +1,26 @@
 class Vacant < Formula
   desc "Fast domain availability checker via authoritative DNS"
   homepage "https://github.com/alltuner/vacant"
-  version "0.4.2"
+  version "0.4.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/alltuner/vacant/releases/download/v0.4.2/vacant-aarch64-apple-darwin.tar.gz"
-      sha256 "2dc5a0badf90cd7e2e6f36187224c105d820676e946efa35d1168d90f91c9dc2"
+      url "https://github.com/alltuner/vacant/releases/download/v0.4.0/vacant-aarch64-apple-darwin.tar.gz"
+      sha256 "0b5128b32e41d35e0205ee1e388e5e4f71b142d44621af7a5d064c6e68a7d336"
     else
-      url "https://github.com/alltuner/vacant/releases/download/v0.4.2/vacant-x86_64-apple-darwin.tar.gz"
-      sha256 "6400966803166318d79db9cfa108ffadc5ae564a8925ec5cae0657bca0960de3"
+      url "https://github.com/alltuner/vacant/releases/download/v0.4.0/vacant-x86_64-apple-darwin.tar.gz"
+      sha256 "23f56c3e975658c75d269a6c9099b814876da411f3d4e680c482dfa91994f28f"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/alltuner/vacant/releases/download/v0.4.2/vacant-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "a11630104b87fed68827acbb045bb2e9e1674eeb4eeecfe36787ae90777a4b6e"
+      url "https://github.com/alltuner/vacant/releases/download/v0.4.0/vacant-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "05f0a2746d5c57e1a76d18a74f1b3c878e7a1a29b6b1a2b1530ad1458d71e850"
     else
-      url "https://github.com/alltuner/vacant/releases/download/v0.4.2/vacant-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "a1dccbba670ba0093399161daf8eefca0ee5aa00698c17c3579e5b4cd689e976"
+      url "https://github.com/alltuner/vacant/releases/download/v0.4.0/vacant-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "d97ba2f8e6af97df9a23de81f0066920d10c6501a6a2e079b0dad6233db68e63"
     end
   end
 

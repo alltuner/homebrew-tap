34bdedfe6017626693843ec3d69d2c565ad31614
David Poblador i Garcia <davidpoblador@users.noreply.github.com>
2026-05-13 14:06:05 +0000

    vacant: update to v0.4.3

--- Formula/vacant.rb
+++ Formula/vacant.rb
@@ -1,26 +1,26 @@
 class Vacant < Formula
   desc "Fast domain availability checker via authoritative DNS"
   homepage "https://github.com/alltuner/vacant"
-  version "0.4.1"
+  version "0.4.3"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/alltuner/vacant/releases/download/vacant-v0.4.1/vacant-aarch64-apple-darwin.tar.gz"
-      sha256 "1ac4241e032423a9c38be0dff2d8d47616187d61beec17b3fe966a0700cb78fe"
+      url "https://github.com/alltuner/vacant/releases/download/vacant-v0.4.3/vacant-aarch64-apple-darwin.tar.gz"
+      sha256 "e677fd68b1e64b4969acf6d7f4d2ad14607d122c175cf0d62f5cc3f3879e7639"
     else
-      url "https://github.com/alltuner/vacant/releases/download/vacant-v0.4.1/vacant-x86_64-apple-darwin.tar.gz"
-      sha256 "01716c1eb743dbb2661e0ebfa48d829c0f467543450c0e24b46f0c63c47dd571"
+      url "https://github.com/alltuner/vacant/releases/download/vacant-v0.4.3/vacant-x86_64-apple-darwin.tar.gz"
+      sha256 "5482a772462e8c2996471431b281964c236cac8b81fc2eea53dffdea17adcec9"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/alltuner/vacant/releases/download/vacant-v0.4.1/vacant-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "6f5fb925d1c1fa7833945c322fe3e58a62c3ac912e5ec8d5642f784b20009769"
+      url "https://github.com/alltuner/vacant/releases/download/vacant-v0.4.3/vacant-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "665aa3e67f2a23d0fdaf198cfbd35a9c38bcd3d4d964b0f3b703e0f62dce0552"
     else
-      url "https://github.com/alltuner/vacant/releases/download/vacant-v0.4.1/vacant-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "634fb3543625675e526bc8fd764936a0834abee03495b37b44133ac9ca5373f8"
+      url "https://github.com/alltuner/vacant/releases/download/vacant-v0.4.3/vacant-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "8a2afba257200eb833239dcec1f853fc0642d84385a3a3614bb86eaaa1ff3069"
     end
   end
 

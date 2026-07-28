c73323384595516c2138ecbec23e580c89d5b2e5
David Poblador i Garcia <davidpoblador@users.noreply.github.com>
2026-07-28 11:01:19 +0000

    vacant: update to v0.4.12

--- Formula/vacant.rb
+++ Formula/vacant.rb
@@ -1,26 +1,26 @@
 class Vacant < Formula
   desc "Fast domain availability checker via authoritative DNS"
   homepage "https://github.com/alltuner/vacant"
-  version "0.4.11"
+  version "0.4.12"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/alltuner/vacant/releases/download/vacant-v0.4.11/vacant-aarch64-apple-darwin.tar.gz"
-      sha256 "5447d3eac388920e8c0edc7b9627212e4659d26162776388dd3d368442d5e1bc"
+      url "https://github.com/alltuner/vacant/releases/download/vacant-v0.4.12/vacant-aarch64-apple-darwin.tar.gz"
+      sha256 "a34f2076924de1ce8943454fe3dcab76a43eca540002d31a31b6b059d207a238"
     else
-      url "https://github.com/alltuner/vacant/releases/download/vacant-v0.4.11/vacant-x86_64-apple-darwin.tar.gz"
-      sha256 "7ae72b70b1de1e3aa9586546b311171095f75586561079a48ae8f879bba042df"
+      url "https://github.com/alltuner/vacant/releases/download/vacant-v0.4.12/vacant-x86_64-apple-darwin.tar.gz"
+      sha256 "f6ccd0316f2c3562b2685a083afcfdc4904be46d7d11a3bae3c51a75f4d8f512"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/alltuner/vacant/releases/download/vacant-v0.4.11/vacant-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "ec679295a858986184f47ecb948177c68fda4d26d90027e972c3ff68a775f11f"
+      url "https://github.com/alltuner/vacant/releases/download/vacant-v0.4.12/vacant-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "6157b2ddc59365a143907e024884450f34c4fddf75952329f3a41eb82b3b3862"
     else
-      url "https://github.com/alltuner/vacant/releases/download/vacant-v0.4.11/vacant-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "983cfcce05d6957cb889a0dbed809527c59a489f8f7ec60a046e214d0a636489"
+      url "https://github.com/alltuner/vacant/releases/download/vacant-v0.4.12/vacant-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "85f333789903772f2f37e39871ba0a3311b4c5437825e9bf2d71e78d20a5747d"
     end
   end
 

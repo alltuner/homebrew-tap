d4c91093dd83ea1d2b8768b4a6b776892921572f
David Poblador i Garcia <davidpoblador@users.noreply.github.com>
2026-07-28 20:44:42 +0000

    mise-completions-sync: update to v0.5.12

--- Formula/mise-completions-sync.rb
+++ Formula/mise-completions-sync.rb
@@ -1,26 +1,26 @@
 class MiseCompletionsSync < Formula
   desc "Sync shell completions for tools managed by mise"
   homepage "https://github.com/alltuner/mise-completions-sync"
-  version "0.5.11"
+  version "0.5.12"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/alltuner/mise-completions-sync/releases/download/v0.5.11/mise-completions-sync-aarch64-apple-darwin.tar.gz"
-      sha256 "37b89385091265c5612d674d1778bda059dcfe4515da83445526fdcc88c05123"
+      url "https://github.com/alltuner/mise-completions-sync/releases/download/v0.5.12/mise-completions-sync-aarch64-apple-darwin.tar.gz"
+      sha256 "abba73174c03abb0fa3713b4a08d2c33e2b791b9cfd247fd7857d15f16a86ca1"
     else
-      url "https://github.com/alltuner/mise-completions-sync/releases/download/v0.5.11/mise-completions-sync-x86_64-apple-darwin.tar.gz"
-      sha256 "8fda4c72a8bcceaab4df23f0b85f2426b661f6369848f648265276d0a5104b32"
+      url "https://github.com/alltuner/mise-completions-sync/releases/download/v0.5.12/mise-completions-sync-x86_64-apple-darwin.tar.gz"
+      sha256 "7c6cd1887976849e18dc4a9ef1a6dae6f982a5b3deb4136342d98ff9bf488f84"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/alltuner/mise-completions-sync/releases/download/v0.5.11/mise-completions-sync-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "1b4ed6d01676e191d0c0d61ac6886bcab15cf89650888cad8fee44bf72ef6935"
+      url "https://github.com/alltuner/mise-completions-sync/releases/download/v0.5.12/mise-completions-sync-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "53a013b1602daee43af4ebe913e89a12fa3f0cf7ce01829e42d8a687f6a9796a"
     else
-      url "https://github.com/alltuner/mise-completions-sync/releases/download/v0.5.11/mise-completions-sync-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "d8d0395b0bf3a18690259e8c6d7c29be21c5d3a45a363d49dbe2e8d01b42ae10"
+      url "https://github.com/alltuner/mise-completions-sync/releases/download/v0.5.12/mise-completions-sync-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "26baf6c296ce4724adadb731fc5f24a39debdf3a74bfe4ef264c879774e1a39e"
     end
   end
 
